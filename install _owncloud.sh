c215a706677c77096463ed015b6bba2db2b82377
NRGLine4Sec <mailforgithub.nrgline@yopmail.com>
2017-05-17 22:01:39 +0200

    Update install _owncloud.sh

--- install _owncloud.sh
+++ install _owncloud.sh
@@ -87,11 +87,13 @@ server {
   }
 
   location ~ \.php(?:$|/) {
+    include snippets/fastcgi-php.conf;
     fastcgi_split_path_info ^(.+\.php)(/.+)$;
     include fastcgi_params;
     fastcgi_param SCRIPT_FILENAME $document_root$fastcgi_script_name;
     fastcgi_param PATH_INFO $fastcgi_path_info;
     fastcgi_param HTTPS on;
+    #fastcgi_pass unix:/var/run/php5-fpm.sock;
     fastcgi_pass php-handler;
     fastcgi_intercept_errors on;
   }
