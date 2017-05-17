489329207b6402ea94ed1b110666a759a6ac0487
NRGLine4Sec <mailforgithub.nrgline@yopmail.com>
2017-05-17 18:06:31 +0200

    Update install _owncloud.sh

--- install _owncloud.sh
+++ install _owncloud.sh
@@ -23,44 +23,26 @@ chown -R www-data:www-data /var/www/html/owncloud/
 echo 'upstream php-handler {
   server 127.0.0.1:9000;
   #server unix:/var/run/php5-fpm.sock;
-  }
-
+}
 server {
   listen 80;
-  server_name owncloud;
-  # Force le passage en https
+  server_name owncloud.memo-linux.com;
+  # enforce https
   return 301 https://$server_name$request_uri;
-  }
 
-server {
-  #listen 443 ssl;
-  #server_name owncloud.example.com;
+  resolver 8.8.8.8;##j'ai mon propre serveur dns en local
 
-  #ssl_certificate /etc/ssl/nginx/owncloud.example.com.crt;
-  #ssl_certificate_key /etc/ssl/nginx/owncloud.example.com.key;
-
-  # Fichier de log
-  access_log /var/log/nginx/ssl.owncloud.example.com.access.log;
-  error_log /var/log/nginx/ssl.owncloud.example.com.error.log;
-
-  # Ajout de header liés à la sécurité
-  add_header Strict-Transport-Security "max-age=15768000; includeSubDomains; preload;";
-  add_header X-Content-Type-Options nosniff;
-  add_header X-Frame-Options "SAMEORIGIN";
-  add_header X-XSS-Protection "1; mode=block";
-  add_header X-Robots-Tag none;
-
-  # Répertoire dans lequel est installé Owncloud
+  # Path to the root of your installation
   root /var/www/html/owncloud/;
-  # Taille de fichier maximum  que lon peut téléverser/uploader
-  client_max_body_size 1G;
+  # set max upload size
+  client_max_body_size 10G;
   fastcgi_buffers 64 4K;
 
-  # Désactivation de la compression pour éviter la suppression du header ETag
+  # Disable gzip to avoid the removal of the ETag header
   gzip off;
 
-  # Décommenter cette option si votre serveur est compilé avec le module ngx_pagespeed
-  # Ce module est non supporté
+  # Uncomment if your server is build with the ngx_pagespeed module
+  # This module is currently not supported.
   #pagespeed off;
 
   rewrite ^/caldav(.*)$ /remote.php/caldav$1 redirect;
@@ -75,42 +57,54 @@ server {
     allow all;
     log_not_found off;
     access_log off;
-    }
+  }
 
   location ~ ^/(?:\.htaccess|data|config|db_structure\.xml|README){
     deny all;
-    }
+  }
 
   location / {
-   # Les régles suivantes sont uniquement nécessaire en cas dutilisation de webfinger
-   rewrite ^/.well-known/host-meta /public.php?service=host-meta last;
-   rewrite ^/.well-known/host-meta.json /public.php?service=host-meta-json last;
+    # The following 2 rules are only needed with webfinger
+    rewrite ^/.well-known/host-meta /public.php?service=host-meta last;
+    rewrite ^/.well-known/host-meta.json /public.php?service=host-meta-json last;
 
-   rewrite ^/.well-known/carddav /remote.php/carddav/ redirect;
-   rewrite ^/.well-known/caldav /remote.php/caldav/ redirect;
+    rewrite ^/.well-known/carddav /remote.php/carddav/ redirect;
+    rewrite ^/.well-known/caldav /remote.php/caldav/ redirect;
 
-   rewrite ^(/core/doc/[^\/]+/)$ $1/index.html;
+    rewrite ^(/core/doc/[^\/]+/)$ $1/index.html;
 
-   try_files $uri $uri/ /index.php;
-   }
+    try_files $uri $uri/ =404;
+  }
 
-   location ~ \.php(?:$|/) {
-   fastcgi_split_path_info ^(.+\.php)(/.+)$;
-   include fastcgi_params;
-   fastcgi_param SCRIPT_FILENAME $document_root$fastcgi_script_name;
-   fastcgi_param PATH_INFO $fastcgi_path_info;
-   fastcgi_param HTTPS on;
-   fastcgi_param modHeadersAvailable true; #Evite denvoyer les header de sécurtié deux fois
-   fastcgi_pass php-handler;
-   }
+  location ~ \.php(?:$|/) {
+    fastcgi_split_path_info ^(.+\.php)(/.+)$;
+    include fastcgi_params;
+    fastcgi_param SCRIPT_FILENAME $document_root$fastcgi_script_name;
+    fastcgi_param PATH_INFO $fastcgi_path_info;
+    fastcgi_param HTTPS on;
+    fastcgi_pass php-handler;
+    fastcgi_intercept_errors on;
+  }
 
-   # Optionnel : positionne un header EXPIRES long sur les ressources statiques
-   location ~* \.(?:jpg|jpeg|gif|bmp|ico|png|css|js|swf)$ {
-       expires 30d;
-       # Optionnel : ne pas logger laccès aux ressources statiques
-         access_log off;
-   }
-  }' >> /etc/nginx/sites-available/owncloud
+  # Adding the cache control header for js and css files
+  # Make sure it is BELOW the location ~ \.php(?:$|/) { block
+  location ~* \.(?:css|js)$ {
+    add_header Cache-Control "public, max-age=7200";
+    # Add headers to serve security related headers
+    add_header Strict-Transport-Security "max-age=15768000; includeSubDomains; preload;";
+    add_header X-Content-Type-Options nosniff;
+    add_header X-Frame-Options "SAMEORIGIN";
+    add_header X-XSS-Protection "1; mode=block";
+    add_header X-Robots-Tag none;
+    # Optional: Don't log access to assets
+    access_log off;
+  }
+
+  # Optional: Don't log access to other assets
+  location ~* \.(?:jpg|jpeg|gif|bmp|ico|png|swf)$ {
+    access_log off;
+  }
+}' >> /etc/nginx/sites-available/owncloud
 
 ln -s /etc/nginx/sites-available/owncloud /etc/nginx/sites-enabled/owncloud
 
